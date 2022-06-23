20e1115c5b94a622e54476a4fdc7b3ab4bf74da9
fatimarenderos <51107094+fatimarenderos@users.noreply.github.com>
2022-06-23 00:12:12 -0600

    Adding login and signup methods

---
+++ login/login.php
@@ -0,0 +1,125 @@
+<?php 
+
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$user_name = $_POST['user_name'];
+		$password = $_POST['password'];
+
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		{
+
+			//read from database
+			$query = "select * from users where user_name = '$user_name' limit 1";
+			$result = mysqli_query($con, $query);
+
+			if($result)
+			{
+				if($result && mysqli_num_rows($result) > 0)
+				{
+
+					$user_data = mysqli_fetch_assoc($result);
+					if(($user_data['user_name'] === 'admin')){
+
+						$_SESSION['user_id'] = $user_data['user_id'];
+						header("Location: ../admin-map.php");
+						die;
+					}
+					if($user_data['password'] === $password)
+					{
+
+						$_SESSION['user_id'] = $user_data['user_id'];
+						header("Location: ../user-map.php");
+						die;
+					}
+					
+				}
+			}
+			
+			echo "wrong username or password!";
+		}else
+		{
+			echo "wrong username or password!";
+		}
+	}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <section class="h-100 gradient-form" style="background-color: #eee;" >
+        <div class="container py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-xl-10">
+              <div class="card rounded-3 text-black">
+                <div class="row g-0">
+                  <div class="col-lg-6">
+                    <div class="card-body p-md-5 mx-md-4">
+      
+                      <div class="text-center">
+                        <img src="https://flexambiental.com/img/Iconos%20flex_12.png"
+                          style="width: 185px;" alt="logo">
+                        <h4 class="mt-1 mb-5 pb-1"> Forestation Tracker </h4>
+                      </div>
+      
+					  <form method="post">
+                        <p>Please login to your account</p>
+      
+                        <div class="form-outline mb-4">
+                          <input type="text" id="user_name" class="form-control"
+                            placeholder="Name" name="user_name"/>
+                          <label class="form-label" for="user_name">Username</label>
+                        </div>
+      
+                        <div class="form-outline mb-4">
+                          <input type="password" id="password" class="form-control" name="password" />
+                          <label class="form-label" for="password">Password</label>
+                        </div>
+
+                        <div class="text-center pt-1 mb-5 pb-1">
+                          <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"  id="button" type="submit" value="Login">Login</button>
+                        </div>
+      
+
+                        <div class="d-flex align-items-center justify-content-center pb-4">
+                        	<p class="mb-0 me-2">Don't have an account?</p>
+						  	<a href="signup.php" type="button" class="btn btn-outline-danger">Click to Signup</a>
+                        </div>
+      
+                      </form>
+      
+                    </div>
+                  </div>
+                  <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                    <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                      <h4 class="mb-4"> Frase equis </h4>
+                      <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+</body>
+</html>
+
